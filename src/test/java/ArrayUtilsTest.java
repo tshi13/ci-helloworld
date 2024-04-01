a2e6fdc14fb7c931c5e109099362d80d16cee21b
tshi13 <tshi13@jh.edu>
2024-04-01 12:12:09 -0400

    add tests

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -45,4 +45,9 @@ public class ArrayUtilsTest {
     public void testOddOrPositiveBothPositivesAndNegatives() {
         assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
     }
+
+    @Test
+    public void testCountOf() {
+        assertEquals(2, ArrayUtils.countOf(new int[]{10, 2, 10, 2}, 10));
+    }
 }
